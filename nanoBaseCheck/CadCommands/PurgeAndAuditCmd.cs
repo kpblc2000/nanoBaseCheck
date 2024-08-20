d53c219b5a528e6ccab375af200ab967724cc50a
kpblc2000 <kpblc2000@gmail.com>
2024-08-20 21:05:15 +0300

    Попытка обратиться к namespace nanoCAD

---
+++ nanoBaseCheck/CadCommands/PurgeAndAuditCmd.cs
@@ -0,0 +1,21 @@
+﻿using HostMgd.ApplicationServices;
+using Teigha.Runtime;
+
+namespace nanoBaseCheck.CadCommands
+{
+    public static class PurgeAndAuditCmd
+    {
+        [CommandMethod("-kpblc-purge-and-audit")]
+        public static void PurgeNadAuditCommand()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            if (doc == null)
+            {
+                return;
+            }
+
+            // Вот здесь ошибка - пространство имен nanoCAD тупо не опознается
+            nanoCAD.Document comDoc = doc.AcadDocument as nanoCAD.Document;
+        }
+    }
+}
